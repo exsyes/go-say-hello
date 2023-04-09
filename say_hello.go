64b258ffba87e0357d5ab3458c33f7b8264fd68e
exsyes <digiverbal2@gmail.com>
2023-04-09 10:00:25 +0700

    menambahkan komentar

--- say_hello.go
+++ say_hello.go
@@ -3,5 +3,5 @@ package go_say_hello
 import "fmt"
 
 func SayHello(name string) string {
-	return fmt.Sprintf("hello my name is %s", name)
+	return fmt.Sprintf("hello my name is %s", name) //return string
 }
